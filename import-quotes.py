9f943a7cd3762a214a317787555b5355661455a5
tfree87 <tlf11@albion.edu>
2015-06-04 11:23:01 -0400

    Added start date argument

--- import-quotes.py
+++ import-quotes.py
@@ -26,48 +26,51 @@ from pandas.io.data import DataReader
 from datetime import datetime
 from datetime import timedelta
 
-def get_prices(security):
+
+def get_prices(security, date):
     """Gets a stock price DataFrame from Yahoo"""
-    start_date = datetime(1900, 1, 1)
+    start_date = datetime.strptime(date, "%Y-%m-%d")
     end_date = datetime.today()
-    result = DataReader(security, 'yahoo', start_date, end_date)
+    result = DataReader(security, "yahoo", start_date, end_date)
     return result
 
-def main(filename, namespace, security):
+
+def main(filename, namespace, security, date="1900-01-01"):
     """The main function"""
     url = "xml://" + filename
-    stock_price = get_prices(security)
+    stock_price = get_prices(security, date)
     
     # Initialize Gnucash session
     session = Session(url, True, False, False)
     book = session.book
     commod_table = book.get_table()
     stock = commod_table.lookup(namespace, security)
-    currency = commod_table.lookup('CURRENCY', 'USD')
+    currency = commod_table.lookup("CURRENCY", "USD")
     pricedb = book.get_price_db()
-    price_list = pricedb.get_prices(stock,currency)
+    price_list = pricedb.get_prices(stock, currency)
 
+    # TODO: Need to find a way to add prices without the need to clone a previous price
     if len(price_list)<1:
-        print('Need at least one database entry to clone ...')
+        print("Need at least one database entry to clone ...")
 
-	# Delete all the old price entries except for the first
-    pl0 = price_list[0]
+    # Delete all the old prices from the list
+    first_price = price_list[0]
     for i in range(0,len(price_list)):
         pricedb.remove_price(price_list[i])
 
     print(stock_price.index)
     for item in stock_price.index:
-        p_new = pl0.clone(book)
-        p_new = gnucash.GncPrice(instance=p_new)
+        current_price = first_price.clone(book)
+        current_price = gnucash.GncPrice(instance=current_price)
         string = "Adding {} {}".format(item, stock_price["Close"][item.strftime("%Y-%m-%d")])
         print(string)
         # Add stock date formatted as year/month/day
-        p_new.set_time(item)
-        v = p_new.get_value()
+        current_price.set_time(item)
+        v = current_price.get_value()
         v.num = int(Fraction.from_float(stock_price["Close"][item.strftime("%Y-%m-%d")]).limit_denominator(100000).numerator)
         v.denom = int(Fraction.from_float(stock_price["Close"][item.strftime("%Y-%m-%d")]).limit_denominator(100000).denominator)
-        p_new.set_value(v)
-        pricedb.add_price(p_new)
+        current_price.set_value(v)
+        pricedb.add_price(current_price)
 
     # Clean up
     session.save()
@@ -76,19 +79,24 @@ def main(filename, namespace, security):
 
 
 def parse_arguments():
-    """Get arguments from command line and store them as a variable"""
+    """Get arguments from command line and store them in an object"""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "filename",
-        help="Name of the gnucash file"
+        help="Name of the gnucash file to which prices will be imported"
     )
     parser.add_argument(
         "namespace",
-        help="The namespace of the security"
+        help="The namespace of the security: i.e. 'AMEX', 'NYSE','FUND', etc."
     )
     parser.add_argument(
         "security",
         help="The name of the security to get price information for"
+    )
+    parser.add_argument(
+        "-s",
+        "--start-date",
+        help="The oldest (first) date from which historical quotes will be obtained formatted as %Y-%m-%d."
     )
     args = parser.parse_args()
     return args
@@ -96,5 +104,8 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.filename, args.namespace, args.security)
+    if args.start_date:
+        main(args.filename, args.namespace, args.security, args.start_date)
+    else:
+        main(args.filename, args.namespace, args.security)
         
